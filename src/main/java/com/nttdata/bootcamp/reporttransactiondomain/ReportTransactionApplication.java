a69973effa87fc1af56c517ae7c0b7eed10cb403
Jimmy Prada <jipragu@emeal.nttdata.com>
2022-06-21 19:58:58 -0500

    initialize project

---
+++ src/main/java/com/nttdata/bootcamp/reporttransactiondomain/ReportTransactionApplication.java
@@ -0,0 +1,16 @@
+package com.nttdata.bootcamp.reporttransactiondomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ReportTransactionApplication.
+ */
+@SpringBootApplication
+public class ReportTransactionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReportTransactionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/reporttransactiondomain/ReportTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.reporttransactiondomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
